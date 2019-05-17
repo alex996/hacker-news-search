9f7c5e56adb755dae8a0f090097241f7791ef087
Alex <alexnezhynsky@gmail.com>
2019-05-16 22:27:27 -0400

    feat: Implement search

--- src/Search.js
+++ src/Search.js
@@ -1,30 +1,69 @@
 import React, { useState } from 'react'
 import clsx from 'clsx'
 
+const API_URL =
+  'https://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=10&query='
+
 const Search = props => {
   const [input, setInput] = useState('')
   const [hasFocus, setHasFocus] = useState(true)
+  const [stories, setStories] = useState([])
+  const [delay, setDelay] = useState()
 
-  const handleInput = e => setInput(e.target.value)
+  const fetchStories = async query => {
+    const res = await fetch(`${API_URL}${query}`)
+
+    const { hits: stories } = await res.json()
+
+    setStories(stories)
+  }
+
+  const handleInput = async e => {
+    const query = e.target.value
+
+    clearTimeout(delay)
+
+    setDelay(setTimeout(() => fetchStories(query), 250))
+
+    setInput(query)
+  }
 
   const handleFocus = () => setHasFocus(true)
 
   const handleBlur = () => setHasFocus(false)
+
+  const hasStories = stories.length !== 0
 
   return (
     <div className={clsx('search', hasFocus && 'focused')}>
       <input
         autoFocus
         value={input}
-        className="input"
+        className={clsx('input', hasStories && 'has-results')}
         placeholder="Search..."
         onChange={handleInput}
         onFocus={handleFocus}
         onBlur={handleBlur}
       />
-      <ul className="results">
-        <li>foo</li>
-      </ul>
+      {hasStories && (
+        <ul className="results">
+          {stories.map(story => (
+            <li key={story.objectID}>
+              <span />{' '}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="title"
+                href={story.url}
+              >
+                <span className="score">{story.points} pts.</span>
+                <span className="title">{story.title}</span>
+                <em className="author">by {story.author}</em>
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
